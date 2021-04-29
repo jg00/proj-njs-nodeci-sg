7279aa078ef93e5b8d79d1b689ca663df993ffe1
Jack Gamboa <jgamboa929@gmail.com>
2021-04-29 12:24:14 -0500

    Update CI cnfig

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+// Commit for CI
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
+  cookieKey: process.env.COOKIE_KEY,
+  redisUrl: "redis://127.0.0.1:6379",
+};
